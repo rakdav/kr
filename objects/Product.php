88104fae30b697739433b8f2fe52af5eac0d9e15
Andrews <rakdav@inbox.ru>
2024-11-30 12:44:35 +0200

    30.11-2

--- objects/Product.php
+++ objects/Product.php
@@ -26,4 +26,23 @@ class Product
         $stmt->execute();
         return $stmt;
     }
+    public function create(){
+        $query="insert into ".$this->table_name." (category_id,product_name,description,price,created) 
+        values (:category_id,:product_name,:description,:price,:created)";
+        $stmt = $this->conn->prepare($query);
+        $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+        $this->product_name=htmlspecialchars(strip_tags($this->product_name));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->price=htmlspecialchars(strip_tags($this->price));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+        $stmt->bindParam(':category_id',$this->category_id);
+        $stmt->bindParam(':product_name',$this->product_name);
+        $stmt->bindParam(':description',$this->description);
+        $stmt->bindParam(':price',$this->price);
+        $stmt->bindParam(':created',$this->created);
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+    }
 }
