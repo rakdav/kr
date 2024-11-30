be9b719d96c2361357d768830b1331b26aab9a62
Andrews <rakdav@inbox.ru>
2024-11-30 13:01:14 +0200

    30.11-3

--- product/create.php
+++ product/create.php
@@ -2,21 +2,20 @@
 
 use objects\Product;
 
-header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, 
-    Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     include_once '../config/database.php';
     include_once '../objects/product.php';
     $database = new Database();
     $db = $database->getConnection();
     $product = new Product($db);
     $data = json_decode(file_get_contents("php://input"));
-    if(!empty($data->name)&&!empty($data->description)
+    if(!empty($data->product_name)&&!empty($data->description)
         &&!empty($data->price)&&!empty($data->category_id)) {
-        $product->name = $data->name;
+        $product->product_name = $data->product_name;
         $product->description = $data->description;
         $product->price = $data->price;
         $product->category_id = $data->category_id;
